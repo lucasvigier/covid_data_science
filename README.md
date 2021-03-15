c2c402063095d068f4e65921c2ef638dcabc287d
lucas-vigier <lucas.vigier@etu.univ-nantes.fr>
2021-03-15 10:21:56 +0100

    df confirmed

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Covid Python Analysis
+# Covid Data Science
+
+Italy, Spain, France, United Kingdom, US, India, Belgium, Russia, Netherlands, Bangladesh
--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 import numpy as np
 import pandas as pd
+
+country_list = ["Italy", "Spain", "France", "United Kingdom", "US", "India", "Belgium", "Russia", "Netherlands", "Bangladesh"]
+total_confirmed_csv = pd.read_csv('csv/time_series_covid19_confirmed_global.csv', index_col=0)
+
+def total_confirmed_country(df):
+    df.set_index("Country/Region", inplace=True)
+    result = df.loc[country_list]
+    print(result)
+
+total_confirmed_country(total_confirmed_csv)
